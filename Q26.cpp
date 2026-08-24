ce014f2b52ef55366cafd4307e53c164244c2208
baswarajswami56 <baswarajswami193@gmail.com>
2026-08-24 09:56:43 +0530

    Add files via upload

---
+++ Q26.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    int length, breadth;
+
+public:
+    // Default constructor
+    Rectangle() {
+        length = breadth = 0;
+    }
+
+    // Parameterized constructor
+    Rectangle(int l, int b) {
+        length = l;
+        breadth = b;
+    }
+
+    // Copy constructor
+    Rectangle(const Rectangle &r) {
+        length = r.length;
+        breadth = r.breadth;
+    }
+
+    void area() {
+        cout << "Area = " << length * breadth << endl;
+    }
+};
+
+int main() {
+    Rectangle r1;       // Default
+    Rectangle r2(10,5); // Parameterized
+    Rectangle r3(r2);   // Copy
+
+    r1.area();
+    r2.area();
+    r3.area();
+
+    return 0;
+}
